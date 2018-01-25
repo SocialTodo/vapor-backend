35f10b326c83ec579896d63436573a7a5c1f4d7c
Saatvik Arya <aryasaatvik@gmail.com>
2018-01-24 23:08:27 -0600

    added shared to todolist

--- Sources/App/Models/TodoModels/TodoList.swift
+++ Sources/App/Models/TodoModels/TodoList.swift
@@ -6,6 +6,7 @@ final class TodoList: Model {
     let storage = Storage()
 
     var title: String
+    var shared: Bool
     var listOwnerId: Identifier
     var listOwner: Parent<TodoList, FacebookUser> {
         return parent(id: listOwnerId)
@@ -17,23 +18,27 @@ final class TodoList: Model {
     enum Keys {
         static let title = "title"
         static let listOwnerId = FacebookUser.foreignIdKey
+        static let shared = "shared"
     }
 
-    init(title:String, listOwner: FacebookUser) {
+    init(title:String, listOwner: FacebookUser, shared: Bool) {
         self.title = title
         // Add error handling if the user hasn't been saved yet
         self.listOwnerId = listOwner.id!
+        self.shared = shared
     }
 
     init(row: Row) throws {
         title = try row.get(Keys.title)
         listOwnerId = try row.get(Keys.listOwnerId)
+        shared = try row.get(Keys.shared)
     }
 
     func makeRow() throws -> Row {
         var row = Row()
         try row.set(Keys.title, title)
         try row.set(Keys.listOwnerId, listOwnerId)
+        try row.set(Keys.shared, shared)
         return row
     }
 }
@@ -44,6 +49,7 @@ extension TodoList: Preparation {
             $0.id()
             $0.string(Keys.title)
             $0.string(Keys.listOwnerId)
+            $0.bool(Keys.shared)
         }
     }
 
